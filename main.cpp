6770fa5bf4a15807465ba94737f55549e5d71c8c
ifkan <127970886+ifkan15@users.noreply.github.com>
2026-03-17 22:26:19 +0300

    добавил код hello World и метод

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int summaty(int a, b, c) {
+	return c = a + b;
+}
+
+int main() {
+	cout << "Hello Warld";
+	cout << "Hello Warld";
+	cout << "Hello Warld";
+
+	cout << summaty(3, 1);
+}
+
